0b56fd9df0532868c48985da694b591ed31e7849
Abill <kurnianabil253@gmail.com>
2024-10-29 19:23:15 +0700

    +create,edit,delete,search

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $prodis = Prodi::orderBy('id', 'desc')->get();
-        return view('prodi.index', compact('prodis'));
+        // Cek apakah ada pencarian
+        $search = $request->input('search');
+
+        if ($search) {
+            // Jika ada, cari berdasarkan nama
+            $prodis = Prodi::where('nama', 'like', '%' . $search . '%')->orderBy('id', 'desc')->get();
+        } else {
+            // Jika tidak ada pencarian, tampilkan semua data
+            $prodis = Prodi::orderBy('id', 'desc')->get();
+        }
+
+        // Kembalikan view dengan data prodis
+        return view('prodi.index', compact('prodis', 'search'));
     }
 
     public function create()
@@ -31,30 +42,32 @@ class ProdiController extends Controller
         // Redirect back to the Prodi index route with a success message
         return redirect()->route('/prodi')->with('success', 'Program Studi berhasil ditambahkan');
     }
+
     public function edit($id)
-{
+    {
         $prodi = Prodi::findOrFail($id);
-    return view('prodi.edit', compact('prodi'));
-}
+        return view('prodi.edit', compact('prodi'));
+    }
 
     public function update(Request $request, $id)
-{
+    {
         $request->validate([
-        'nama' => 'required'
+            'nama' => 'required'
         ]);
 
         $prodi = Prodi::findOrFail($id);
         $prodi->update([
-        'nama' => $request->nama
+            'nama' => $request->nama
         ]);
 
-    return redirect()->route('/prodi')->with('success', 'Program Studi berhasil diupdated');
-}
+        return redirect()->route('/prodi')->with('success', 'Program Studi berhasil diupdate');
+    }
+
     public function delete($id)
-{
+    {
         $prodi = Prodi::findOrFail($id);
         $prodi->delete();
 
-    return redirect()->route('/prodi')->with('success', 'Data Program Studi berhasil dihapus');
-}
+        return redirect()->route('/prodi')->with('success', 'Data Program Studi berhasil dihapus');
+    }
 }
--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -17,8 +17,10 @@
                         @endif
                         <div class="ml-auto d-flex">
                             <a href="{{ route('prodi.create') }}" class="btn btn-primary mr-2">Tambah Program Studi</a>
-                            <form action="" method="GET" class="d-flex">
-                                <input type="text" name="search" class="form-control" placeholder="Pencarian">
+                            
+                            <!-- Search Form -->
+                            <form action="{{ route('/prodi') }}" method="GET" class="d-flex">
+                                <input type="text" name="search" class="form-control" placeholder="Pencarian" value="{{ old('search', $search) }}">
                                 <button class="btn btn-primary ml-2" type="submit">
                                     <i class="bi bi-search"></i>
                                 </button>
@@ -26,6 +28,7 @@
                         </div>
                     </div>
 
+                    <!-- Program Studi Table -->
                     <table class="table table-hover">
                         <thead class="table-primary">
                             <tr>
@@ -51,6 +54,13 @@
                             @endforeach
                         </tbody>
                     </table>
+
+                    <!-- Back Button after Search moved to bottom left -->
+                    @if(request()->has('search'))
+                        <div class="mt-4">
+                            <a href="{{ route('/prodi') }}" class="btn btn-secondary">Back</a>
+                        </div>
+                    @endif
 
                 </div>
             </div>
